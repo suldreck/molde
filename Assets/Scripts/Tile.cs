d87816367ddc0a57d4456be7c3b993e57be164de
suldreck <suldreck@hotmail.com>
2018-06-02 13:32:06 +0200

    base

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+
+    public Tile[] NextTiles;
+    public PlayerStone PlayerStone;
+    public bool IsScoringSpace;
+    public bool IsRollAgain;
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
