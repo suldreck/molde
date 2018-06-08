272a2b1e1e9a49b51c06e5b8def34e3bafab47c4
suldreck <suldreck@hotmail.com>
2018-06-08 12:32:01 +0200

    se ve oscuro
    voy a quitar las luces de la proteccion a ver si se reconstruyen

--- Assets/noDestruir.cs
+++ Assets/noDestruir.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class noDestruir : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        DontDestroyOnLoad(gameObject);
+    public static noDestruir juegoVariable;
+    private void Awake()
+    {
+        
+        if(juegoVariable ==null)
+        {juegoVariable=this;
+            DontDestroyOnLoad(gameObject);
+         }else if(juegoVariable!=this)//no es null pero tampoco es lo q teniamos antes  
+            {
+            Destroy(gameObject);
+            }
+    }
+    // Use this for initialization
+    void Start () {
+    
 	}
 	
 	// Update is called once per frame
