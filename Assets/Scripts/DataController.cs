6616ead44edb95dc1f93564efaf14bbeab603385
suldreck <suldreck@hotmail.com>
2018-06-12 22:42:32 +0200

    He añadido para leer las preguntas por json

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class DataController : MonoBehaviour 
 {
-	public RoundData[] allRoundData;
-	
-	void Start ()  
+	private RoundData[] allRoundData;
+	private string gameDataFileName = "QuestionsAndAnswers.json";
+    void Start ()  
 	{
 		DontDestroyOnLoad (gameObject);
-
+        LoadGame();
         //SceneManager.LoadScene ("MenuScreen");
         SceneManager.LoadScene("oca");
     }
@@ -18,4 +19,19 @@ public class DataController : MonoBehaviour
 	{
 		return allRoundData [0];
 	}
+
+    private void LoadGame()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName); 
+        if(File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("No se puede cargar las preguntas");
+        }
+    }
 }
